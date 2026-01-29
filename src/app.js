f585ad08944c00a549bc525eaa706d1a2da346fc
Harshon20 <singhharsh18433@gmail.com>
2026-01-29 13:03:25 +0530

    new commit

--- src/app.js
+++ src/app.js
@@ -14,9 +14,13 @@ app.use(cors({
 app.use(express.json({limit: "20kb"}));
 app.use(express.urlencoded({extended: true, limit: "20kb"}))
 app.use(express.static("public"))
-
 app.use(cookieParser());
 
+//routes import 
+import userRoutes from "./routes/user.routes.js";
 
+//routes declaration
+//app.use("/users", userRoutes);
+app.use("/api/v1/users", userRoutes);
 
 export { app };
